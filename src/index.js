8332d5e81ba179aaa99dd3913743afd59a4a2bc9
Theis Nielsen <apollo@theiss-mbp.lan>
2020-04-02 16:18:30 +0100

    fixed export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import RoomAndTransactionView from "./components/room/RoomAndTransactionView.vue";
 import ConfirmationView from "./components/confirmation/ConfirmationView.vue";
+import BreadCrumbs from "./components/shared/BreadCrumbs.vue";
 
-export { RoomAndTransactionView, ConfirmationView };
+export { RoomAndTransactionView, ConfirmationView, BreadCrumbs };
