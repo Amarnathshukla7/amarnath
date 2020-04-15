a9995b56c08b15f6fb881985d22c9d083bec764c
Theis Nielsen <theissnielsen@gmail.com>
2020-04-15 12:28:24 +0100

    colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,7 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-const stc = {
+export const stc = {
   primary: "#062131",
   secondary: "#ff8700",
   accent: "#2aa6b9",
@@ -17,7 +17,7 @@ const stc = {
   greyback: "#f8f8f8",
 };
 
-const fp = {
+export const fp = {
   primary: "#4e5b67",
   secondary: "#ff6016",
   accent: "#bfc8c5",
@@ -30,14 +30,42 @@ const fp = {
   greyback: "#f8f8f8",
 };
 
+export const cph = {
+  primary: "#454544",
+  secondary: "#4fb696",
+  accent: "#4fb696",
+  error: "#FF5252",
+  info: "#e4e6ea",
+  other: "#dcf3f7",
+  success: "#4CAF50",
+  warning: "#eb3a3a",
+  greyish: "#899495",
+  greyback: "#f8f8f8",
+};
+
+export const nh = {
+  primary: "#454544",
+  secondary: "#199dda",
+  accent: "#199dda",
+  error: "#FF5252",
+  info: "#e4e6ea",
+  other: "#dcf3f7",
+  success: "#4CAF50",
+  warning: "#eb3a3a",
+  greyish: "#899495",
+  greyback: "#f8f8f8",
+};
+
 export default new Vuetify({
   theme: {
     options: {
       customProperties: true,
     },
     themes: {
-      light: stc,
+      // light: stc,
       // light: fp,
+      // light: cph,
+      light: nh,
     },
   },
   icons: {
