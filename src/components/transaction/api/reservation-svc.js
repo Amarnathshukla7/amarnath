5f7e5899e5cf3eb98a3b8acc8f8b64b0509f33c9
Theis <theissnielsen@gmail.com>
2020-04-30 13:29:32 +0100

    added access token to request

--- src/components/transaction/api/reservation-svc.js
+++ src/components/transaction/api/reservation-svc.js
@@ -3,5 +3,12 @@ import { get } from "idb-keyval";
 
 export const create = async (data) => {
   const { token } = await get("token");
-  return axios.post(`/reservation-svc/${token}`, data).then((res) => res.data);
+  const accessToken = localStorage.getItem("auth._token.local");
+  return axios
+    .post(`/reservation-svc/${token}`, data, {
+      headers: {
+        ...(accessToken && { Authorization: accessToken }),
+      },
+    })
+    .then((res) => res.data);
 };
