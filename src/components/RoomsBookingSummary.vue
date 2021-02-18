2a89af3d84368baef47be3353b327cf296dee7b6
George Kirby <georgekirby@Georges-MacBook-Pro.local>
2021-02-18 15:11:45 +0000

    translation style adjustment

--- src/components/RoomsBookingSummary.vue
+++ src/components/RoomsBookingSummary.vue
@@ -135,13 +135,13 @@
       class="white--text pt-4 total-price-room-summary"
     >
       <v-row no-gutters class="mb-5">
-        <v-col cols="6">
+        <v-col cols="7">
           <div class="heading ml-6 mb-md-6 font-weight-bold">
             {{ uiContent.totalPrice }}:
           </div>
         </v-col>
 
-        <v-col cols="6">
+        <v-col cols="5">
           <div class="heading text-right mr-6 font-weight-bold">
             {{ cost | formatPrice(currency) }}
           </div>
