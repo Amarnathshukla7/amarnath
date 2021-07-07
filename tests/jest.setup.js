a9ec120512185194c5cb6df3d524fae7c8b25072
Ersin Demirtas <ersin.demirtas@bedsandbars.com>
2021-07-07 16:37:19 +0100

    bumped the version 0.10.21

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import '@testing-library/vue'
-import '@testing-library/jest-dom/extend-expect'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "@testing-library/vue";
+import "@testing-library/jest-dom/extend-expect";
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
--- tests/unit/components/ConfirmationEasterEggOverlay.spec.js
+++ tests/unit/components/ConfirmationEasterEggOverlay.spec.js
@@ -1,23 +1,23 @@
-import { render, fireEvent } from '@testing-library/vue'
+import { render, fireEvent } from "@testing-library/vue";
 
 import ConfirmationEasterEggOverlay from "@/components/ConfirmationEasterEggOverlay.vue";
 
 describe("ConfirmationEasterEggOverlay.vue", () => {
   it("should render overlay with close button and iframe", () => {
-    const { getByTestId } = render(ConfirmationEasterEggOverlay)
+    const { getByTestId } = render(ConfirmationEasterEggOverlay);
 
-    expect(getByTestId('ceeo_overlay')).toBeVisible()
-    expect(getByTestId('ceeo_iframe')).toBeVisible()
-    expect(getByTestId('ceeo_btn')).toBeVisible()
+    expect(getByTestId("ceeo_overlay")).toBeVisible();
+    expect(getByTestId("ceeo_iframe")).toBeVisible();
+    expect(getByTestId("ceeo_btn")).toBeVisible();
   });
 
   it("should emits close-overlay event when close icon is clicked", async () => {
-    const { getByTestId, emitted } = render(ConfirmationEasterEggOverlay)
-    const closeButton = getByTestId('ceeo_btn')
+    const { getByTestId, emitted } = render(ConfirmationEasterEggOverlay);
+    const closeButton = getByTestId("ceeo_btn");
 
-    expect(emitted()).not.toHaveProperty('close-overlay')
-    fireEvent.click(closeButton)
+    expect(emitted()).not.toHaveProperty("close-overlay");
+    fireEvent.click(closeButton);
 
-    expect(emitted()).toHaveProperty('close-overlay')
+    expect(emitted()).toHaveProperty("close-overlay");
   });
 });
