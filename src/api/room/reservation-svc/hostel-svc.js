53362e0fe6de57a24e5f4ab06b0de0866cd5e526
ersin-demirtas <ersin.demirtas@bedsandbars.comm>
2021-07-21 12:56:48 +0100

    removed unused indexedDB write

--- src/api/room/reservation-svc/hostel-svc.js
+++ src/api/room/reservation-svc/hostel-svc.js
@@ -3,6 +3,5 @@ import { set } from "idb-keyval";
 
 export const find = async (code) =>
   axios.get(`/hostel-svc/${code}`).then(async (res) => {
-    await set(`hostel-config.${code}`, res.data);
     return res.data;
   });
