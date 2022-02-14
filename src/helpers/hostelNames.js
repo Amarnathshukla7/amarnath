c4f0e4f7c99235541b0451579c65d6d185f0ccce
ed-bbwmc <83449957+ed-bbwmc@users.noreply.github.com>
2022-02-14 11:19:41 +0000

    added vienna and bedwood hostel short names (#38)

--- src/helpers/hostelNames.js
+++ src/helpers/hostelNames.js
@@ -7,7 +7,8 @@ export const hostelShortName = (hostelCode) => {
     BER: "Berlin Alexanderplatz Hostel",
     BRA: "Berlin Alexanderplatz Apartments",
     BRM: "Berlin Mitte Hostel",
-    COP: "Copenhagen Downtown Hostel",
+    COP: "Copenhagen - Downtown Hostel",
+    BWD: "Copenhagen - Bedwood hostel",
     PRA: "Prague - The Czech Inn",
     PAR: "Paris Canal Hostel",
     GDN: "Paris Gare Du Nord Hostel",
@@ -24,6 +25,7 @@ export const hostelShortName = (hostelCode) => {
     CMD: "London - Camden Hostel",
     SHP: "London - Shepherd's Bush Hostel",
     HMM: "London - Hammersmith Hostel",
+    VIE: "Vienna - Vienna Hostel",
   };
 
   const shortName = hostels[hostelCode];
